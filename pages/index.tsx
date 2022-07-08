4b5799749f6c1194924a7d9ae827671429ed566e
Pawel Kaminski <pawelkaminski@tipi.software>
2022-07-09 01:21:45 +0200

    Add simplest FSM

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,31 @@
+import { useMachine } from "@xstate/react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { myMachine } from "../machines/myFistMachine";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return <div className={styles.container}>Hello world</div>;
+  const [state, send] = useMachine(myMachine);
+  return (
+    <div className={styles.container}>
+      {JSON.stringify(state.value)}
+      <button
+        onClick={() => {
+          send("MOUSEOVER");
+        }}
+      >
+        Mouse over
+      </button>
+      <button
+        onClick={() => {
+          send("MOUSEOUT");
+        }}
+      >
+        Mouse out
+      </button>
+    </div>
+  );
 };
 
 export default Home;
